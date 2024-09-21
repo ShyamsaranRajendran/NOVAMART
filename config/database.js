a6f211ddbd812f2aa8266b3296cbc3f5f9365371
ShyamsaranRajendran <sarancode6@gamil.com>
2024-09-21 20:54:04 +0530

    final commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports={
-    database:'mongodb://localhost/cmscart'
+    database:'mongodb+srv://sarancode6:phwAi0IYGLd5aWQM@test-pro-db.bptyj9z.mongodb.net/novamart'
+    // mongodb+srv://sarancode6:<db_password>@test-pro-db.bptyj9z.mongodb.net/?retryWrites=true&w=majority&appName=test-pro-db
 }
